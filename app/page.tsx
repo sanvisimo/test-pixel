a868d03d2bafea1295ade20d3a73ae606d8b3ac2
Simone Sanvito <sanvi.simo@gmail.com>
2024-10-30 18:26:30 +0100

    add image

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,64 @@
 'use client'
 
 import Image from "next/image";
-import {useMemo, useState} from "react";
+import { useState} from "react";
+import {SimpleDialog} from "@/components/SimpleDialog";
+
+function getRandomIndex(usedIndexs: number[], maxIndex: number) {
+  const min = 0;
+  const max = maxIndex - 1;
+  let index = Math.floor(Math.random()*(max-min+1)+min);
+
+  return usedIndexs.includes(index) ? index++ : index
+}
 
 export default function Home() {
-  const [pixels, setPixels] = useState<number>(1100)
-  const pixelArray = useMemo(() => Array.from({length: pixels}, (_, i) => i), [pixels])
+  const [open, setOpen] = useState(false);
+  const [pixels, setPixels] = useState<number[]>([])
 
-  const handleClick =(pixel = 1 )=> {
-    setPixels(prevPixels => prevPixels === 1000*1000 ? prevPixels : prevPixels + pixel)
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const generateNumbers = async (numbers: number) => {
+    let p = [...pixels]
+      for(let i = 0; i < numbers; i++) {
+      console.log('p', p)
+      const index = getRandomIndex(p, 1000000);
+      p = [...p, index];
+    }
+
+      setPixels(p)
   }
+
+  const handleClose = (numbers: number) => {
+    setOpen(false);
+    console.log('pixels', numbers)
+    generateNumbers(numbers)
+  };
+
+  console.log('poixe', pixels)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl">Oscura il comunismo!</h1>
+      <main className="flex  gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col gap-8">
+        <h1 className="text-5xl">Cancel wokiness!</h1>
         <div className="flex">
           <button type="button"
-                  onClick={() => handleClick(1)}
+                  onClick={handleClickOpen}
                   className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-            Compra un pixel
+            Buy pixels
           </button>
-
-          <button type="button"
-                  onClick={() => handleClick(100)}
-                  className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-            Compra 100 pixel
-          </button>
-          <button type="button"
-                  onClick={() => handleClick(1000)}
-                  className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-            Compra 1000 pixel
-          </button>
+          <SimpleDialog
+              open={open}
+              onClose={handleClose}
+          />
+        </div>
         </div>
         <div>
           <div style={{width: 1000, height: 1000, position: 'relative', border: "1px solid red"}}>
-            {pixelArray.map(pixel => {
+            {pixels.map(pixel => {
                 const row = Math.ceil(pixel / 1000) -1;
               const col = (pixel % 999);
 
@@ -51,11 +74,10 @@ export default function Home() {
                 />)
             })}
             <Image
-                src="/pixel_test.png"
+                src="/digital_woke.webp"
                 alt="Example Image"
                 width={1000}
                 height={1000}
-
             />
           </div>
         </div>
---
+++ components/SimpleDialog.tsx
@@ -0,0 +1,74 @@
+import {
+     Button,
+    Dialog,
+    DialogActions, DialogContent, DialogContentText,
+    DialogTitle,
+     TextField
+} from "@mui/material";
+
+
+
+export interface SimpleDialogProps {
+    open: boolean;
+    onClose: (pixels: number) => void;
+}
+
+
+export function SimpleDialog(props: SimpleDialogProps) {
+    const { onClose,  open } = props;
+
+    const handleClose = () => {
+        onClose(0);
+    };
+
+    const onPixelSelect =  (pixels: number) => {
+        onClose(pixels);
+    };
+
+
+    return (
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+                component: 'form',
+                onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
+                    event.preventDefault();
+                    const formData = new FormData(event.currentTarget);
+                    const formJson = Object.fromEntries((formData as any).entries());
+                    const pixels = formJson.pixels;
+                    console.log(pixels);
+                    onPixelSelect(pixels);
+                },
+            }}
+        >
+            <DialogTitle>Cancel woke culture!</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    To Cancel the woke please subscribe! <br />
+                    1€ for pixel
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    required
+                    margin="dense"
+                    id="pixels"
+                    name="pixels"
+                    label="pixels"
+                    type="number"
+                    slotProps={{
+                        inputLabel: {
+                            shrink: true,
+                        },
+                    }}
+                    fullWidth
+                    variant="standard"
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Subscribe</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
